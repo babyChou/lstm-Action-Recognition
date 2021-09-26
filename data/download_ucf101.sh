6ad2713fca1af3a54ec50144f544c06fec3aa7ab
Anita.Chou <Anita.Chou@avermedia.com>
2021-09-26 19:09:14 +0800

    Update download_ucf101.sh location

--- data/download_ucf101.sh
+++ data/download_ucf101.sh
@@ -2,5 +2,6 @@
 
 # Downloads the UCF-101 dataset
 
-wget https://www.crcv.ucf.edu/data/UCF101/UCF101.rar --no-check-certificate
+# wget https://www.crcv.ucf.edu/data/UCF101/UCF101.rar --no-check-certificate
+wget http://storage.googleapis.com/thumos14_files/UCF101_videos.zip
 wget https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip --no-check-certificate
