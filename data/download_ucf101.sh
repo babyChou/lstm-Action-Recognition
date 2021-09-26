46cc92926d9ee312ad1bec6153de10e830d6522a
Anita.Chou <Anita.Chou@avermedia.com>
2021-09-26 18:30:46 +0800

    update files

---
+++ data/download_ucf101.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Downloads the UCF-101 dataset
+
+wget https://www.crcv.ucf.edu/data/UCF101/UCF101.rar --no-check-certificate
+wget https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip --no-check-certificate
---
+++ data/extract_frames.py
@@ -0,0 +1,52 @@
+"""
+Helper script for extracting frames from the UCF-101 dataset
+"""
+
+import av
+import glob
+import os
+import time
+import tqdm
+import datetime
+import argparse
+
+
+def extract_frames(video_path, _):
+    frames = []
+    video = av.open(video_path)
+    for frame in video.decode(0):
+        yield frame.to_image()
+
+
+prev_time = time.time()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, default="UCF101", help="Path to UCF-101 dataset")
+    opt = parser.parse_args()
+    print(opt)
+
+    time_left = 0
+    video_paths = glob.glob(os.path.join(opt.dataset_path, "**", "*.avi"), recursive=True)
+    for i, video_path in enumerate(video_paths):
+        # print(111, video_path.split(".avi")[0])
+        sequence_type, sequence_name = video_path.split(".avi")[0].split("\\")[-2:]
+        sequence_path = os.path.join(f"{opt.dataset_path}-frames__", sequence_type, sequence_name)
+
+        if os.path.exists(sequence_path):
+            continue
+
+        os.makedirs(sequence_path, exist_ok=True)
+
+        # Extract frames
+        for j, frame in enumerate(
+            tqdm.tqdm(
+                extract_frames(video_path, time_left),
+                desc=f"[{i}/{len(video_paths)}] {sequence_name} : ETA {time_left}",
+            )
+        ):
+            frame.save(os.path.join(sequence_path, f"{j}.jpg"))
+
+        # Determine approximate time left
+        videos_left = len(video_paths) - (i + 1)
+        time_left = datetime.timedelta(seconds=videos_left * (time.time() - prev_time))
+        prev_time = time.time()
